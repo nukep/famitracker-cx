5f97d711aaa6de7df099060920aefc149f020658
Dan Spencer <dan@atomicpotato.net>
2013-01-01 08:15:45 -0700

    soundthread: Adjusting source code indent

--- src/sound/soundthread.cpp
+++ src/sound/soundthread.cpp
@@ -2,68 +2,68 @@
 #include <boost/thread/mutex.hpp>
 #include "soundthread.hpp"
 
-	SoundThread::SoundThread()
-		: m_thread(NULL), m_running(false)
-	{
-	}
-	SoundThread::~SoundThread()
-	{
-		wait();
-	}
+SoundThread::SoundThread()
+	: m_thread(NULL), m_running(false)
+{
+}
+SoundThread::~SoundThread()
+{
+	wait();
+}
 
-	void SoundThread::run(callback_t f, void *data)
+void SoundThread::run(callback_t f, void *data)
+{
+	m_mtx_running.lock();
+	if (m_thread != NULL)
 	{
-		m_mtx_running.lock();
-		if (m_thread != NULL)
-		{
-			if (m_running)
-			{
-				_wait_nomtx();
-			}
-			_delthread();
-		}
-
-		m_running = true;
-		m_thread = new boost::thread(_job, this, f, data);
-		m_mtx_running.unlock();
-	}
-	void SoundThread::wait()
-	{
-		m_mtx_running.lock();
-		_wait_nomtx();
-		m_mtx_running.unlock();
-	}
-	void SoundThread::_wait_nomtx()
-	{
-		if (m_thread == NULL)
-			return;
-
 		if (m_running)
 		{
-			if (m_thread->get_id() == boost::this_thread::get_id())
-			{
-				// waiting on the current thread will deadlock
-				return;
-			}
-
-			// unlock the mutex because _job acquires it
-			m_mtx_running.unlock();
-			m_thread->join();
-			m_mtx_running.lock();
+			_wait_nomtx();
 		}
 		_delthread();
 	}
 
-	void SoundThread::_job(SoundThread *t, callback_t f, void *data)
-	{
-		(*f)(data);
-		t->m_mtx_running.lock();
-		t->m_running = false;
-		t->m_mtx_running.unlock();
-	}
+	m_running = true;
+	m_thread = new boost::thread(_job, this, f, data);
+	m_mtx_running.unlock();
+}
+void SoundThread::wait()
+{
+	m_mtx_running.lock();
+	_wait_nomtx();
+	m_mtx_running.unlock();
+}
+void SoundThread::_wait_nomtx()
+{
+	if (m_thread == NULL)
+		return;
 
-	void SoundThread::_delthread()
+	if (m_running)
 	{
-		delete m_thread;
-		m_thread = NULL;
+		if (m_thread->get_id() == boost::this_thread::get_id())
+		{
+			// waiting on the current thread will deadlock
+			return;
+		}
+
+		// unlock the mutex because _job acquires it
+		m_mtx_running.unlock();
+		m_thread->join();
+		m_mtx_running.lock();
 	}
+	_delthread();
+}
+
+void SoundThread::_job(SoundThread *t, callback_t f, void *data)
+{
+	(*f)(data);
+	t->m_mtx_running.lock();
+	t->m_running = false;
+	t->m_mtx_running.unlock();
+}
+
+void SoundThread::_delthread()
+{
+	delete m_thread;
+	m_thread = NULL;
+}
--- src/sound/soundthread.hpp
+++ src/sound/soundthread.hpp
@@ -9,23 +9,23 @@ namespace boost
 	class mutex;
 }
 
-	class SoundThread
-	{
-	public:
-		typedef void (*callback_t)(void*);
-		SoundThread();
-		~SoundThread();
-		void run(callback_t f, void *data);
-		void wait();
-	private:
-		boost::thread *m_thread;
-		boost::mutex m_mtx_running;
-		bool m_running;
+class SoundThread
+{
+public:
+	typedef void (*callback_t)(void*);
+	SoundThread();
+	~SoundThread();
+	void run(callback_t f, void *data);
+	void wait();
+private:
+	boost::thread *m_thread;
+	boost::mutex m_mtx_running;
+	bool m_running;
 
-		void _wait_nomtx();
-		static void _job(SoundThread *t, callback_t f, void *data);
-		void _delthread();
-	};
+	void _wait_nomtx();
+	static void _job(SoundThread *t, callback_t f, void *data);
+	void _delthread();
+};
 
 #endif
 
